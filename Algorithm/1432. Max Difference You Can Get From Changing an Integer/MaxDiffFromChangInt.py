ae58971d34e1e36edafc26694c3158786e827c9f
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-06-29 21:09:28 -0400

    Update MaxDiffFromChangInt.py

--- Algorithm/1432. Max Difference You Can Get From Changing an Integer/MaxDiffFromChangInt.py
+++ Algorithm/1432. Max Difference You Can Get From Changing an Integer/MaxDiffFromChangInt.py
@@ -6,25 +6,31 @@ referred to https://youtu.be/sOnpt2zHWfQ
 class Solution:
     def maxDiff(self, num: int) -> int:
         
-        small = 1<< 60
+        # there is only 100 possibilities for x and y's permutations
+        
+        small = 1 << 60
         big = 0
+        
+        # x and y are both 0 - 9
         
         for x in range(10):
             for y in range(10):
                 k = str(num).replace(str(x), str(y))
                 
-                # test if the new integer is 0 or have leading 0's
-                
+        # check if there are leading zeros or if the new integer is 0
+        
                 if k[0] == '0':
                     continue
                 if int(k) == 0:
                     continue
+                    
                 k = int(k)
-                
+
                 small = min(k, small)
                 big = max(k, big)
                 
         return big - small
+    
         
         
     
