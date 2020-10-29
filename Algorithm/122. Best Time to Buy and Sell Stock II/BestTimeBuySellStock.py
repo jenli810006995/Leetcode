1a2f9357953bb0d9733322888b3525274b7a12d8
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-10-29 07:44:04 -0400

    Update BestTimeBuySellStock.py
    added another try on Leetcode #122

--- Algorithm/122. Best Time to Buy and Sell Stock II/BestTimeBuySellStock.py
+++ Algorithm/122. Best Time to Buy and Sell Stock II/BestTimeBuySellStock.py
@@ -14,3 +14,16 @@ class Solution:
                 maxprofit += prices[i] - prices[i - 1]
         return maxprofit
     
+
+# Time: 10/29/2020
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        total = 0
+            for i in range(1, len(prices)):
+                if prices[i] > prices[i - 1]:
+                    total += prices[i] - prices[i - 1]
+#                   total += max(prices[i] - prices[i - 1], 0)
+        return total
+
+# Reference: https://youtu.be/PtK--dyWob4
