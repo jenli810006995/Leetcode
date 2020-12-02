9a446690d5d1e514da9397c41939fdce143d4340
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-12-02 08:48:30 -0500

    Update reverseSinglyList.java

--- Algorithm/Mock Interview/Reverse Singly List/reverseSinglyList.java
+++ Algorithm/Mock Interview/Reverse Singly List/reverseSinglyList.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/* iterative */
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
@@ -20,4 +22,16 @@ class Solution {
         }
         return prev;
     }
+}
+
+/* recursive */
+
+class Solution{
+    public ListNode reverseList(ListNode head){
+        if (head == null || head.next == null) return head;
+            ListNode p = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return p;
+     }   
 }
